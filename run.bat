5c76a74e32122732a6bc3b2e29c9911874452b9e
soramkirtep <soramkirtep@gmail.com>
2020-09-21 16:37:32 +0200

    changes to bat file

--- run.bat
+++ run.bat
@@ -8,7 +8,7 @@ echo off
 ::SchTasks /Create /RU [your_user] /RP [your_password] /SC DAILY /TN "certbot_ssl_updater" /TR "%JBOSS_HOME%\..\scripts\certbot_ssl_updater\run.bat %JBOSS_HOME%\..\scripts\certbot_ssl_updater\ python" /ST 01:20 /RL HIGHEST /F
 cd %JBOSS_HOME%\..\scripts\certbot_ssl_updater\
 python main.py
-
+pause
 exit
 
 
